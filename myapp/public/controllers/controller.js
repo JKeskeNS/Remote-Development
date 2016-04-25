d681fadc5552c71e7f29735182cc64c318dd8c08
JKeskeNS <Jkeske@niceshoes.com>
2016-04-25 12:06:18 -0500

    More cleanup

--- myapp/public/controllers/controller.js
+++ myapp/public/controllers/controller.js
@@ -1,7 +1,6 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
     
     var refresh = function () {
         $http.get('/api/users').success(function (response) {
